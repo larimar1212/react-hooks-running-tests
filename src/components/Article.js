19c47d3b8a2c13dd44c44c1905b3a15cb10287e6
larimar <larimarfondeur@gmail.com>
2022-08-05 10:23:54 -0400

    idontgetit

--- src/components/Article.js
+++ src/components/Article.js
@@ -3,7 +3,15 @@ import React from "react";
 function Article() {
   return (
     <div>
-      {/* please pass this test */}
+      test{("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  // add this line
+  screen.debug();
+
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+})}
+
     </div>
   );
 }
